04b790c0f0ba5b575f93da4a58647ed3b0d1d3a5
SM Emon Islam <73327452+smemonislam@users.noreply.github.com>
2023-10-28 00:50:16 +0600

    Update View Page role

--- public/dashboard/user/view.php
+++ public/dashboard/user/view.php
@@ -45,7 +45,7 @@ try {
                     <div class="card-body text-center">
                         <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp" alt="avatar" class="rounded-circle img-fluid" style="width: 150px;">
                         <h5 class="my-3"><?php echo $item['username']; ?></h5>
-                        <p class="text-muted mb-1">Full Stack Developer</p>
+                        <p class="text-muted mb-1"><?php echo ucwords($item['role']); ?></p>
                         <p class="text-muted mb-4">Bay Area, San Francisco, CA</p>
                         <div class="d-flex justify-content-center mb-2">
                             <button type="button" class="btn btn-primary">Follow</button>
